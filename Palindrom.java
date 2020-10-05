4e928190c411af76be2dbf1398c1f7baf2e1f892
Rudolf Mato <rudolf.mato@kosickaakademia.sk>
2020-10-05 12:37:08 +0200

    tu je palindrom

---
+++ Palindrom.java
@@ -0,0 +1,26 @@
+package sk.kosickaakademia.matorudolf;
+import java.util.Scanner;
+public class Palindrom {
+    public static void main(String[] args) {
+        System.out.print("Zadaj čislo: ");
+        Scanner sc = new Scanner(System.in);
+                long x = sc.nextLong();
+                long x2 = x;
+                long y = 0;
+
+                while (x2 > 0) {
+                    y = y + (x2 % 10);
+                    y = y * 10;
+                    x2 = (x2 / 10);
+                }
+                y = y / 10;
+                if(x==y){
+                    System.out.println("Toto cislo je palindrom");
+                }
+                else{
+                    System.out.println("Toto cislo neni palindrom");
+                }
+            }
+        }
+
+
